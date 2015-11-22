f8eec0224d337b2f2d94bc30962be5a7fd0c65e7
cndesantana <charles.desantana@eawag.ch>
2015-11-22 21:25:02 +0100

    Update testplot in order to plot figures with white background

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,23 @@
+Pkg.checkout("Vega")
+
 using RobHood 
 using Vega
 using Quandl
 
 function testplot()
     println("Testing RobHood.jl");
-    googledat = quandl("GOOG/NASDAQ_QQQ",rows=4218,format="DataFrame")
-    amazondat = quandl("YAHOO/AMZN",rows=4218,format="DataFrame")
+    googledat = quandl("GOOG/NASDAQ_QQQ",rows=4218,format="DataFrame");
+    amazondat = quandl("YAHOO/AMZN",rows=4218,format="DataFrame");
     
-    x1=collect(1:size(googledat[1,],1))
-    y1=collect(googledat[5,])
+    x1=collect(1:size(googledat[1,],1));
+    y1=collect(googledat[5,]);
 
-    x2=collect(1:size(amazondat[1,],1))
-    y2=collect(amazondat[5,])
+    x2=collect(1:size(amazondat[1,],1));
+    y2=collect(amazondat[5,]);
 
-    x = [1:length(x1); 1:length(x2)]
-    y = [y1; y2]
-    group = [[1 for i in 1:length(x1)]; [2 for i in 1:length(x2)]]
+    x = [1:length(x1); 1:length(x2)];
+    y = [y1; y2];
+    group = [[1 for i in 1:length(x1)]; [2 for i in 1:length(x2)]];
     
     googleplot=lineplot(x = x, y = y, group = group)
    
@@ -23,6 +25,7 @@ function testplot()
     ylab!(googleplot,title="Closed")
     title!(googleplot,title="Alphabet(1) and Amazon(2) (1999-2015)")
     googleplot.background = "white"
+    googleplot;
 end
 
 testplot()
